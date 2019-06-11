25847c83a4f725653a0d8c2bf9d14ea85c270534
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-06-11 13:47:22 +0900

    helloword以外をコメントアウト

--- main.go
+++ main.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo"
-	"github.com/oribe1115/Todo-list-server/handler"
-	"github.com/oribe1115/Todo-list-server/model"
 )
 
 func main() {
-	_, err := model.EstablishConnection()
-	if err != nil {
-		log.Fatal("Cannot Connect to Database: %s", err)
-	}
+	// _, err := model.EstablishConnection()
+	// if err != nil {
+	// 	log.Fatal("Cannot Connect to Database: %s", err)
+	// }
 
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World.\n")
 	})
-	e.GET("create/table", handler.CreateTableHandler)
+	// e.GET("create/table", handler.CreateTableHandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
