23cc662960e5e35e0295918a463d898061d324e0
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-06-11 13:35:24 +0900

    portの設定を忘れていた

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
+	"github.com/oribe1115/Todo-list-server/handler"
+	"github.com/oribe1115/Todo-list-server/model"
 )
 
 func main() {
-	// _, err := model.EstablishConnection()
-	// if err != nil {
-	// 	log.Fatal("Cannot Connect to Database: %s", err)
-	// }
+	_, err := model.EstablishConnection()
+	if err != nil {
+		log.Fatal("Cannot Connect to Database: %s", err)
+	}
 
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World.\n")
 	})
-	// e.GET("create/table", handler.CreateTableHandler)
+	e.GET("create/table", handler.CreateTableHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4000"
+	}
+
+	e.Start(":" + port)
 }
