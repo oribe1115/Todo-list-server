3b80e24e6766b150845c3245136958430b7fa0a6
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-06-11 12:31:10 +0900

    db接続のを書いた

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/oribe1115/Todo-list-server/model"
+)
+
+func main() {
+	_, err := model.EstablishConnection()
+	if err != nil {
+		log.Fatal("Cannot Connect to Database: %s", err)
+	}
 }
