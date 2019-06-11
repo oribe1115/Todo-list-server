c2d32cc1543812fcf8f786226de0b4efd960ff4c
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-06-11 12:23:00 +0900

    パッケージの構成を作った

---
+++ model/model.go
@@ -0,0 +1,24 @@
+package model
+
+import (
+	"errors"
+	"os"
+
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	db          *gorm.DB
+	detabaseURL string
+)
+
+func EstablishConnection() (*gorm.DB, error) {
+	detabaseURL = os.Getenv("DATABASE_URL")
+	_db, err := gorm.Open("postgres", detabaseURL)
+	if err != nil {
+		return nil, errors.New("faild to connect to DB")
+	}
+	db = _db
+
+	return db, nil
+}
